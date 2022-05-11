0efdd6c07c4fc1eca03d5d6238fe439f80f51577
zhs007 <sssxueren@gmail.com>
2022-05-11 17:11:29 +0800

    upd

--- examples/simatk/main.go
+++ examples/simatk/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/zhs007/simatk/battle"
 
 func main() {
-	battle.Sim(200, 10, 10, "基础战斗模拟", "report.json")
+	battle.Sim(300, 10, 10, "基础战斗模拟", "report.json")
 }
--- examples/simatk2/main.go
+++ examples/simatk2/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/zhs007/simatk/battle2"
 
 func main() {
-	battle2.Sim(300, 10, 10, 10, "基础战斗模拟2", "report2.json")
+	battle2.Sim(150, 10, 10, 10, "基础战斗模拟2", "report2.json")
 }
